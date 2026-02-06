2d9a3b06c1e41a6fb4e554a53ed55ef20642f93c
Majid Seifi Kashani <64505231+majidseifi@users.noreply.github.com>
2026-02-06 16:17:02 -0500

    Time: 36 ms (95.40%) | Memory: 55.1 MB (97.43%) - LeetSync

--- 2798-chunk-array/chunk-array.js
+++ 2798-chunk-array/chunk-array.js
@@ -4,11 +4,7 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
-    if (arr.length === 0) {
-        return [];
-    }
-
-    result = []
+    const result = []
 
     for (let i = 0; i < arr.length; i+= size) {
         result.push(arr.slice(i, i + size));
