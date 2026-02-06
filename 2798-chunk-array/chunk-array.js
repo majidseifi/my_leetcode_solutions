c3a84655b7c42c77b8c032122c7d9374e2659004
Majid Seifi Kashani <64505231+majidseifi@users.noreply.github.com>
2026-02-06 16:16:15 -0500

    Time: 59 ms (7.91%) | Memory: 56.3 MB (57.47%) - LeetSync

---
+++ 2798-chunk-array/chunk-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    if (arr.length === 0) {
+        return [];
+    }
+
+    result = []
+
+    for (let i = 0; i < arr.length; i+= size) {
+        result.push(arr.slice(i, i + size));
+    }
+
+    return result;
+};
