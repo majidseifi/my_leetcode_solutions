2dc9d35a1f5a1ef7b829d0ffc3ab6ae6fe0d550f
Majid Seifi Kashani <64505231+majidseifi@users.noreply.github.com>
2026-02-11 13:56:55 -0500

    Time: 3 ms (54.77%) | Memory: 68.7 MB (12.35%) - LeetSync

--- 189-rotate-array/rotate-array.ts
+++ 189-rotate-array/rotate-array.ts
@@ -4,11 +4,17 @@
 function rotate(nums: number[], k: number): void {
     const n = nums.length
     k %= n;
+    
+    reverse(nums, 0, n - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, n - 1);
 
-    const doubleNums = [...nums, ...nums];
-
-    const result = doubleNums.slice(n - k, n - k + n);
-    for (let i = 0; i< n; i++) {
-        nums[i] = result[i]
+    function reverse(nums: number[], start: number, end: number) {
+        while (start < end) {
+            [nums[start], nums[end]] = [nums[end], nums[start]];
+            start++;
+            end--;
+        }
     }
+
 };
