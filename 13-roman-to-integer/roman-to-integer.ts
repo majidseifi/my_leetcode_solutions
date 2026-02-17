894293d7dd67fda5c8facee48dcbc04e32b43798
Majid Seifi Kashani <64505231+majidseifi@users.noreply.github.com>
2026-02-17 12:31:16 -0500

    Time: 4 ms (76.55%) | Memory: 61 MB (90.18%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.ts
@@ -0,0 +1,21 @@
+function romanToInt(s: string): number {
+    let res = 0;
+    const letters = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        if (letters[s[i]] < letters[s[i+1]]) {
+            res -= letters[s[i]];
+        } else {
+            res += letters[s[i]];
+        }
+    }
+    return res;
+};
