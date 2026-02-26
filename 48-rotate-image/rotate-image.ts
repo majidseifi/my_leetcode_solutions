540db53c0dcbbd87bddd95b17a7b2972234a1723
Majid Seifi Kashani <64505231+majidseifi@users.noreply.github.com>
2026-02-25 21:06:11 -0500

    Time: 0 ms (100.00%) | Memory: 55.3 MB (90.00%) - LeetSync

---
+++ 48-rotate-image/rotate-image.ts
@@ -0,0 +1,11 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = i; j < matrix.length; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+    matrix.forEach(arr => arr.reverse());
+};
