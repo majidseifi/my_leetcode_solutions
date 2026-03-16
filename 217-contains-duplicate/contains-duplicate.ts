4dfa5a2be6f9c70e27e8b0a49b957e43ed2a3422
Majid Seifi Kashani <64505231+majidseifi@users.noreply.github.com>
2026-03-16 11:56:37 -0400

    Time: 14 ms (86.98%) | Memory: 72.9 MB (91.26%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -0,0 +1,12 @@
+function containsDuplicate(nums: number[]): boolean {
+    const seen = new Set<number>();
+
+    for (const num of nums) {
+        if (seen.has(num)) {
+            return true;
+        } else {
+            seen.add(num);
+        }
+    }
+    return false;
+};
