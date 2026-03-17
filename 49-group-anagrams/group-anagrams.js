d2b0a724b7b0c53f5bca652b971e661c82972530
Majid Seifi Kashani <64505231+majidseifi@users.noreply.github.com>
2026-03-17 16:29:27 -0400

    Time: 62 ms (8.92%) | Memory: 68.3 MB (45.09%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const res = {};
+    for (let s of strs){
+        let count = new Array(26).fill(0);
+        const sortedS = s.split('').sort().join('');
+        for (let i = 0; i < sortedS.length; i++) {
+            count[sortedS.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        }
+        const key = count.join(',');
+        if(!res[key]) {
+            res[key] = [];
+        }
+        res[key].push(s);
+    }
+    return Object.values(res);
+};
