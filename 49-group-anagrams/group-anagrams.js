51cb05d9e3e57cfd934f9bba5a82f3e3f3dc3de5
Majid Seifi Kashani <64505231+majidseifi@users.noreply.github.com>
2026-03-17 16:29:47 -0400

    Time: 38 ms (36.81%) | Memory: 68.8 MB (32.20%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -6,9 +6,8 @@ var groupAnagrams = function(strs) {
     const res = {};
     for (let s of strs){
         let count = new Array(26).fill(0);
-        const sortedS = s.split('').sort().join('');
-        for (let i = 0; i < sortedS.length; i++) {
-            count[sortedS.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        for (let i = 0; i < s.length; i++) {
+            count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
         }
         const key = count.join(',');
         if(!res[key]) {
