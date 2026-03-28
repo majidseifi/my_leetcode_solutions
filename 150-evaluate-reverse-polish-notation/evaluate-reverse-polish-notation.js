6bfd382bc4f4ebf13bf2fc20dd41420b55f782e1
Majid Seifi Kashani <64505231+majidseifi@users.noreply.github.com>
2026-03-28 17:01:10 -0400

    Time: 183 ms (7.57%) | Memory: 57.7 MB (61.00%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    while (tokens.length > 1) {
+        for (let i = 0; i < tokens.length; i++) {
+            if ('+-*/'.includes(tokens[i])) {
+                const a = parseInt(tokens[i - 2]);
+                const b = parseInt(tokens[i - 1]);
+                let result;
+                if (tokens[i] === '+') result = a + b;
+                else if (tokens[i] === '-') result = a - b;
+                else if (tokens[i] === '*') result = a * b;
+                else if (tokens[i] === '/') result = Math.trunc(a / b);
+
+                tokens.splice(i - 2, 3, result.toString());
+                break;
+            }
+        }
+    }
+    return parseInt(tokens[0]);
+};
