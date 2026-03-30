0471f7e05e3c5ec25468950714a683d191d03dba
Majid Seifi Kashani <64505231+majidseifi@users.noreply.github.com>
2026-03-30 11:43:33 -0400

    Time: 37 ms (25.39%) | Memory: 84.1 MB (7.48%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    const res = new Array(temperatures.length).fill(0);
+    const stack = [];
+
+    for (let i = 0; i < temperatures.length; i++) {
+        const t = temperatures[i];
+        while (stack.length > 0 && t > stack[stack.length - 1][0]) {
+            const [stackT, stackInd] = stack.pop();
+            res[stackInd] = i - stackInd;
+        }
+        stack.push([t, i]);
+    }
+    return res;
+};
