9a2b3435243173c3e330a3b4f4c7012a70471f95
Majid Seifi Kashani <64505231+majidseifi@users.noreply.github.com>
2026-04-17 20:45:08 -0400

    Time: 7 ms (12.10%) | Memory: 66.6 MB (9.49%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxP = 0;
+    let minBuy = prices[0];
+
+    for (let sell of prices) {
+        maxP = Math.max(maxP, sell - minBuy);
+        minBuy = Math.min(minBuy, sell);
+    }
+    return maxP;
+};
